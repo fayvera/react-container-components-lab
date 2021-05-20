18e7f8cea16dd82e13182aebcf56787238be0f8f
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-20 15:46:09 -0400

    commit tree 672e1ba43d447a66d1527fd5c4709694d32393b7

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,41 @@
 // Code MovieReviews Here
+import React from 'react'
+import { render } from "enzyme"
+
+const Review = ({
+    byline,
+    critics_pick,
+    date_updated,
+    display_title,
+    headline,
+    link,
+    mpaa_rating,
+    multimedia,
+    publication_date,
+    summary_short
+}) => {
+    return (
+        <div 
+        className="review"
+        key={headline}>
+            <header>
+                <a className="review-link" href={link.url}>{headline}</a>
+            </header>
+            <br/>
+            <span className="author">{byline}</span>
+            <blockquote className="summary">{summary_short}</blockquote>
+            <br></br>
+        </div>
+    )
+}
+
+
+const MovieReviews = ({reviews}) => <div className = "review-list">
+    {reviews.map(Review)}
+</div>
+
+MovieReviews.defaultProps = {
+    reviews: []
+  };
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,40 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component{
+    state  ={
+        reviews: [],
+        searchTerm: ''
+    }
+    
+
+    handleSubmit = event => {
+        event.preventDefault()
+        fetch(URL.concat(this.state.searchTerm))
+        .then(resp => resp.json())
+        .then(response => this.setState({
+            reviews: response.results
+        }))
+    }
+
+    handleInputSearch = (event) => {
+        this.setState({ searchTerm: event.target.value})
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Search Movie Reviews: </label>
+                    <input type="text" onChange={this.handleInputSearch}></input>
+                    <button type="submit">Submit</button>
+                </form>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
